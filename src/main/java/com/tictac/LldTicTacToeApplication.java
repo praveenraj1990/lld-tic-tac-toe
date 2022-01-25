1399560613481565970bedb5d3975f6af8c4b660
Prince <praveen0raj@gmail.com>
2022-01-25 18:31:03 +0530

    Inital changes

---
+++ src/main/java/com/tictac/LldTicTacToeApplication.java
@@ -0,0 +1,13 @@
+package com.tictac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LldTicTacToeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LldTicTacToeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tictac/LldTicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tictac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldTicTacToeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
